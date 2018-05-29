6fcc6bb8e6adeb953e72fd35681eb509b8938e43
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-29 21:03:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+# name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge = []
+    attendees.each do |name|
+    badge << badge_maker(name)
+  end
+  return badge
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+    attendees.each_with_index do |item, index|
+      room_assignments.push << "Hello, #{item}! You'll be assigned to room #{index + 1}!"
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+    badges.each do |name|
+      # binding.pry
+      puts name
+    end
+    # attendees.each_index do |speaker|
+    #   puts assign_rooms(attendees)
+    # end
+end
